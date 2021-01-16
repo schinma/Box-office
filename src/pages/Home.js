f07ae193f469b101f228826f036fdda40e5b7bbb
DESKTOP-6LF5RBE\Marie <schindler.marie@gmail.com>
2021-01-16 20:34:45 +0100

    added input for search and called Google Book API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-    return (
-        <MainPageLayout>
-          This is Home  
-        </MainPageLayout>
-    )
+
+  const [input, setInput] = useState('');
+
+  const onInputChange = (ev) => {
+    setInput(ev.target.value);
+  }
+
+  const onSearch = () => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}&key=AIzaSyCjXeTkWHBuciqAqWjPPXwlhUzlxRG-Dxo`)
+    .then(response => response.json())
+    .then (result => {
+      console.log(result);
+    }); 
+  }
+
+  const onKeyDown = (ev) => {
+    if (ev.onKeyCode === 13) {
+      onSearch();
+    }
+  }
+
+  return (
+      <MainPageLayout>
+        <input type='text' onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+        <button type='button' onClick={onSearch}>Search</button>
+      </MainPageLayout>
+  )
 }
 
 export default Home;
