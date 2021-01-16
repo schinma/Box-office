b7d209db8b22f4b3fb483b02db081ca6ae30f133
DESKTOP-6LF5RBE\Marie <schindler.marie@gmail.com>
2021-01-16 02:18:57 +0100

    added router, navbar and page components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+import Navs from './components/Navs';
 
 function App() {
   return (
-    <div >
-      hi
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path='/'><Home /></Route>
+        <Route exact path='/starred'><Starred /></Route>
+        <Route>
+          <div>
+            Page not Found
+          </div>
+        </Route>
+      </Switch>
     </div>
   );
 }
