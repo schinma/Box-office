f8fa5663ca2b04cbfff0c20faefeb3ba539a5c9a
DESKTOP-6LF5RBE\Marie <schindler.marie@gmail.com>
2021-01-19 21:32:29 +0100

    managing loading time and errors to the book page

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const API_BASE_URL = "https://www.googleapis.com";
 
 export async function apiGET(queryString) {
     
-    const result =  await fetch(`${API_BASE_URL}${queryString}`).then(response => response.json())
+    const url = `${API_BASE_URL}${queryString}`
+    const result =  await fetch(url).then(response => response.json()).catch(error => { throw error });
     return result;
 }
--- src/pages/Book.js
+++ src/pages/Book.js
@@ -6,21 +6,54 @@ const Book = () => {
 
     const { id } = useParams();
     const [book, setBook] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect( () => {
+
+        let isMounted = true;
+
         apiGET(`/books/v1/volumes/${id}`)
         .then( result => {
-            setBook(result);
-            console.log(book);
+            if (isMounted) {
+                setBook(result);
+                setIsLoading(false);
+            }
+        })
+        .catch(err => {
+            if (isMounted) {
+                setError(err.message);
+                setIsLoading(false);
+            }
         });
+        return () => {
+            isMounted = false;
+        }
     }, 
     [id]);
 
+    if (isLoading) {
+        return (
+            <div>
+                Data is being loaded
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div>
+                Error occured : {error};
+            </div>
+        )
+    }
+    
     return (
         <div>
-            book
+            book :
         </div>
     )
 }
+
 
 export default Book
